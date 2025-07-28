463e44af699e2cfb4c1c24cfd51f9ce1a0d146cc
Clayton Acerbi Goncalves <clayton.acerbi1975@gmail.com>
2025-07-28 06:51:06 +0100

    new file

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, blank=True)
+    create_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+
+    
+
+
